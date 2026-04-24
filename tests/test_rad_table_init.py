99c78d59308153af52f7925ffd332f65e3933ae6
Tom-Schaechtel <tstomte@gmail.com>
2026-04-24 16:32:18 +0200

    Minor global state pollution bug fix
    Fixed a minor bug related to global state pollution of atomic property tables.
    Description:
    The global atomic property tables are not reevaluted in subsequent
    calculations if only using default zeo++ provided values.
    
    Example:
    Reading an AtomNetwork from a file using default Zeo++ atomic radii does not correctly reinitialize the default atomic radii if an AtomNetwork was read in beforehand using custom atomic radii.
    
    Code to reproduce:
    from pyzeo.netstorage import AtomNetwork
    from pyzeo.extension import lookupRadius
    atmnet = AtomNetwork.read_from_CSSR("MgO_vac1.cssr", rad_file="MgO.rad")
    atmnet = AtomNetwork.read_from_CSSR("EDI.cssr")
    
    element = "O"
    encoded_str = element.encode("utf-8")
    
    element_radius = lookupRadius(encoded_str)
    
    assert element_radius == 1.52, \
        f"Wrong element radius. Expected 1.52, got {element_radius}"
    
    Example error output:                                                                                             AssertionError: Wrong element radius. Expected 1.52, got 1.84
    
    Comment:
    A test "test_rad_table_init.py" was added for validation.

---
+++ tests/test_rad_table_init.py
@@ -0,0 +1,35 @@
+import pytest
+from pyzeo.netstorage import AtomNetwork
+from pyzeo.extension import lookupRadius
+
+@pytest.mark.parametrize("filename, read_kwargs, expected_value", [
+ 
+    (
+        "MgO_vac1.cssr",
+        {"rad_file": "MgO.rad"},
+        1.84,
+        ),
+
+    (
+        "EDI.cssr",
+        {},
+        1.52,
+        ),
+
+])
+def test_rad_table_init(filename, read_kwargs, expected_value):
+    """
+    Test for global state pollution of the atomic radii table
+    when running sequential calculations with different
+    atomic radii for the same atom types.
+    """
+    atmnet = AtomNetwork.read_from_CSSR(filename, **read_kwargs)
+
+    element = "O"
+    encoded_str = element.encode("utf-8")
+
+    element_radius = lookupRadius(encoded_str)
+
+    assert element_radius == expected_value, \
+            f"Wrong element radius. Expected {expected_value}, got {element_radius}"
+
